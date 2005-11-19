3e7e0dfdf7129347229f81204c08b678ba65fb0a
brio1337 <>
2005-11-19 00:10:10 +0000

    Adding missing file

---
+++ Pipe/FolderInfo.h
@@ -0,0 +1,47 @@
+#pragma once
+
+enum GETINFOFORFOLDER
+{
+	GIFF_SCANNING,
+	GIFF_DIRTY,
+	GIFF_CLEAN
+};
+
+struct FOLDERINFO {
+	ULONGLONG nSize;
+	ULONG nFiles;
+	ULONG nFolders;
+
+	FOLDERINFO() {
+		ZeroMemory(this, sizeof(*this));
+	}
+
+	bool operator==(const FOLDERINFO& Info) const {
+		return !memcmp(this, &Info, sizeof(*this));	}
+
+	bool operator!=(const FOLDERINFO& Info) const {
+		return !operator==(Info); }
+
+	FOLDERINFO& operator+=(const FOLDERINFO& Info) {
+		nSize += Info.nSize;
+		nFiles += Info.nFiles;
+		nFolders += Info.nFolders;
+		return *this;
+	}
+
+	FOLDERINFO& operator-=(const FOLDERINFO& Info) {
+		nSize -= Info.nSize;
+		nFiles -= Info.nFiles;
+		nFolders -= Info.nFolders;
+		return *this;
+	}
+};
+
+struct FOLDERINFO2 : public FOLDERINFO {
+	GETINFOFORFOLDER giff;
+
+	FOLDERINFO2() {
+		ZeroMemory(this, sizeof(*this));
+	}
+};
+
