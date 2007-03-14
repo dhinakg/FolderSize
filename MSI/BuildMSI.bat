75478c80a2ab18487aa44d0da5b768557d40e94d
kirillmueller <>
2007-03-14 12:03:24 +0000

    Allow spaces in the parent folder file name. Use 'rem' instead of ';'. Run with @echo off.

--- MSI/BuildMSI.bat
+++ MSI/BuildMSI.bat
@@ -1,15 +1,16 @@
-; Create or overwrite the MSI from the tables
+@echo off
+rem Create or overwrite the MSI from the tables
 
-msidb -c -dFolderSize.msi -f%cd%\tables *
+msidb -c -d FolderSize.msi -f "%cd%\tables" *
 
-; Create a CAB containing the three FolderSize files
+rem Create a CAB containing the three FolderSize files
 
 cabarc n FolderSize.cab ..\FolderSizeColumn\release\FolderSizeColumn.dll ..\FolderSizeCpl\release\FolderSize.cpl ..\FolderSizeService\release\FolderSizeSvc.exe
 
-; Add the CAB to the MSI as a stream
+rem Add the CAB to the MSI as a stream
 
 msidb -d FolderSize.msi -a FolderSize.cab
 
-; Delete the CAB
+rem Delete the CAB
 
 del FolderSize.cab
